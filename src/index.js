ee2ad91ab2235ed0a2f97819578930cb88df7c02
Manikant Jha <being.manikantjha@gmail.com>
2024-05-31 11:11:01 +0530

    taught components and props

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
 
-const toDisplay = (
-  <div>
-    <nav></nav>
-    <div>
-      <img src="https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1575&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      <h1>Hello World!</h1>
-      <p>
-        n publishing and graphic design, Lorem ipsum is a placeholder text
-        commonly used to demonstrate the visual form of a document or a typeface
-        without relying on meaningful content. Lorem ipsum may be used as a
-        placeholder before the final copy is available.
-      </p>
-    </div>
-    <footer></footer>
-  </div>
-);
+// function myComponent(msg, name, age) {
+//   return <h1>Hello {msg}!</h1>;
+// }
+
+function Message(props) {
+  return <h1>Hello {props.msg}</h1>;
+}
+
+function MyComponent(props) {
+  return <Message msg={props.msg} />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("my-element"));
-root.render(toDisplay);
+root.render(
+  <>
+    <MyComponent msg="Hello" />
+    <MyComponent msg="Class" />
+    <MyComponent msg="123" />
+    {/* {myComponent("World")}
+    {myComponent("Class")}
+    {myComponent("123")} */}
+  </>
+);
