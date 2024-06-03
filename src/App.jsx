8ee75ecde3f532f8b8b52a020edd61aa13a2850c
Manikant Jha <being.manikantjha@gmail.com>
2024-06-03 11:30:50 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Content from "./Content";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import React from "react";
-
-// function myComponent(msg, name, age) {
-//   return <h1>Hello {msg}!</h1>;
-// }
-
-function Message(props) {
-  return <h1>Hello {props.msg}</h1>;
-}
-
-function MyComponent(props) {
-  return <Message msg={props.msg} />;
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("my-element"));
 root.render(
   <>
-    <MyComponent msg="Hello" />
-    <MyComponent msg="Class" />
-    <MyComponent msg="123" />
-    {/* {myComponent("World")}
-    {myComponent("Class")}
-    {myComponent("123")} */}
+    <App />
   </>
 );
