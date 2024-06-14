971b34c3c2666a81db3fd5ed94cb07dcf4bd2b9b
Manikant Jha <being.manikantjha@gmail.com>
2024-06-14 11:20:39 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,20 +16,19 @@ function Home() {
         title="About Us"
         desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae quisquam quae pariatur reprehenderit laudantium itaque, maiores, magni modi dolor, libero tempore aliquid velit beatae exercitationem nam consequuntur ipsam architecto."
         img="https://images.unsplash.com/photo-1718174405504-6c65225786de?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        // imageFirst={false}
+        imageFirst={false}
       />
       <InfoRow
         img="https://images.unsplash.com/photo-1718143047302-56837e2bf688?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Our Story"
         desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae quisquam quae pariatur reprehenderit laudantium itaque, maiores, magni modi dolor, libero tempore aliquid velit beatae exercitationem nam consequuntur ipsam architecto."
-        // imageFirst={true}
         imageFirst
       />
       <InfoRow
         img="https://images.unsplash.com/photo-1718143110625-c83b4d3726a1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Our Journey"
         desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam recusandae quisquam quae pariatur reprehenderit laudantium itaque, maiores, magni modi dolor, libero tempore aliquid velit beatae exercitationem nam consequuntur ipsam architecto."
-        // imageFirst={false}
+        imageFirst={false}
       />
       <Footer />
     </div>
